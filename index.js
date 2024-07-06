58127d320cf6406c5b539c9cd4f14617b3d4b7fb
owl099 <alokk.maurya21@gmail.com>
2024-07-06 12:00:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const dice = document.getElementById('dice-inside');
+const adviceId = document.getElementById('adviceId');
+const advice = document.getElementById('advice');
+
+function fetchAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => {
+            advice.innerHTML = data.slip.advice;
+            adviceId.innerHTML = data.slip.id
+        })
+}
+
+dice.addEventListener('click', () => {
+    fetchAdvice();
+});
+
+
+window.addEventListener('load', fetchAdvice());
