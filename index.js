c72a725247b62ed18aa7d379896794a12a2cae09
owl099 <alokk.maurya21@gmail.com>
2024-09-04 00:09:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const dice = document.getElementById('dice-inside');
 const adviceId = document.getElementById('adviceId');
 const advice = document.getElementById('advice');
-let displayElement = document.getElementById('display');
 
 function fetchAdvice() {
-fetch('https://api.adviceslip.com/advice')
-.then(response => response.json())
-.then(data => {
-advice.innerHTML = data.slip.advice;
-adviceId.innerHTML = data.slip.id
-})
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => {
+            advice.innerHTML = data.slip.advice;
+            adviceId.innerHTML = data.slip.id
+        })
+}
 
 dice.addEventListener('click', () => {
-fetchAdvice();
+    fetchAdvice();
 });
+
 
 window.addEventListener('load', fetchAdvice());
